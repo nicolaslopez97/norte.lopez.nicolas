3eb96108633ba9a3f27433c665d1a95c65d26018
Nicolas <nicolas.lopez97@hotmail.com>
2020-05-04 19:10:45 -0300

    Se agrega proyecto con ejercicios de HTML y se corrigen links a los ejercicios de CSS

--- EjerciciosCss/Ejercicios de CSS/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ejercicios_de_CSS.MvcApplication" Language="C#" %>
---
+++ EjerciciosCss/Ejercicios/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejercicios.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio1()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio2()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio3()
+        {
+            return View();
+        }
+
+        public ActionResult EjercicioEnlaces()
+        {
+            return View();
+        }
+    }
+}
